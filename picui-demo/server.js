0dfb72cd6f3b57f1f4accdc78f7a8ba860886b43
Kevin Hwang <k3vinhwang@gmail.com>
2012-05-23 16:49:44 -0700

    Demo now relays POST requests.

--- picui-demo/server.js
+++ picui-demo/server.js
@@ -14,7 +14,7 @@ server.set('views', __dirname + "/views");
 server.use("/static", express.static(__dirname + "/static"));
 
 //pointer to the piqui-core server
-var coreServer = ""; 
+var coreServer = 'null'; 
 
 server.get("/", function (req, res) {
 	res.render("index");
@@ -24,16 +24,29 @@ server.post("/add", function (req, res) {
 	console.log("Adding file");
 	var url = req.body.url;
 	console.log(url);
-	//todo: relay this to piqui-core
-
+	//relay to picui-core and then send back the body, which will be OK or something
+	request.post({
+		uri: coreServer+"/add",
+		body: JSON.stringify({'url' : url})
+	}, function (error, response, body) {
+		console.log("sent woo");
+		res.send(body); //maybe should be res.send(response)?
+	});
 });
 
 server.post("/match", function (req, res) {
-	console.log("Matching color");
+	console.log("Matching color(s)");
 	var colors = req.body.colors;
 	console.log(colors);
-	//tidiL relay this to piqui-core
-})
+	//relay this to piqui-core, send back a list of images
+	request.post({
+		uri: coreServer+"/match",
+		body: JSON.stringify({'colors' : colors})
+	}, function (error, response, body) {
+		console.log("sent");
+		res.send(body);
+	});
+});
 
 server.listen(80)
 console.log("Express server started");
