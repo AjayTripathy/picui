385ea0b0f29d61039a83db8a474e3766c4aeda21
Kevin Hwang <k3vinhwang@gmail.com>
2012-05-23 01:43:06 -0700

    Added request.js dependency

--- README.md
+++ README.md
@@ -7,8 +7,11 @@ Demo Specifications
 =====
 
 ejs v0.7.1
+
 express v3.0.0alpha1
 
+request 2.9.202
+
 How to run:
-1. `npm install ejs@0.7.1 express@3.0.0alpha1`
+1. `npm install ejs@0.7.1 express@3.0.0alpha1 request@2.9.202`
 2. `sudo node server.js`
--- picui-demo/server.js
+++ picui-demo/server.js
@@ -1,6 +1,9 @@
 var express = require('express');
+var request = require('request');
 
 var server = express.createServer();
+
+server.use(express.bodyParser());
 
 server.set('view options', {
 	layout: false
@@ -10,9 +13,27 @@ server.set('view engine', 'ejs');
 server.set('views', __dirname + "/views");
 server.use("/static", express.static(__dirname + "/static"));
 
+//pointer to the piqui-core server
+var coreServer = ""; 
+
 server.get("/", function (req, res) {
 	res.render("index");
 });
+
+server.post("/add", function (req, res) {
+	console.log("Adding file");
+	var url = req.body.url;
+	console.log(url);
+	//todo: relay this to piqui-core
+
+});
+
+server.post("/match", function (req, res) {
+	console.log("Matching color");
+	var colors = req.body.colors;
+	console.log(colors);
+	//tidiL relay this to piqui-core
+})
 
 server.listen(80)
 console.log("Express server started");
