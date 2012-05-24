c6df3e08ccfd7472c5c2a8e0dc6c1adf6fb5208d
Kevin Hwang <k3vinhwang@gmail.com>
2012-05-23 18:54:14 -0700

    Added socket.io, updated README. Different users get a different tree by default

--- README.md
+++ README.md
@@ -8,10 +8,12 @@ Demo Specifications
 
 ejs v0.7.1
 
-express v3.0.0alpha1
+express v2.5.8
 
-request 2.9.202
+request v2.9.202
+
+socket.io v0.9.6
 
 How to run:
-1. `npm install ejs@0.7.1 express@3.0.0alpha1 request@2.9.202`
+1. `npm install ejs@0.7.1 express@2,5,8 request@2.9.202 socket.io@0.9.6`
 2. `sudo node server.js`
--- picui-demo/server.js
+++ picui-demo/server.js
@@ -1,7 +1,13 @@
 var express = require('express');
 var request = require('request');
+var socketio = require('socket.io');
 
 var server = express.createServer();
+
+var clients = {}
+
+var io = socketio.listen(server);
+io.set('log level', 1);
 
 server.use(express.bodyParser());
 
@@ -14,7 +20,7 @@ server.set('views', __dirname + "/views");
 server.use("/static", express.static(__dirname + "/static"));
 
 //pointer to the piqui-core server
-var coreServer = 'null'; 
+var coreServer = 'http://www.google.com'; 
 
 server.get("/", function (req, res) {
 	res.render("index");
@@ -23,30 +29,43 @@ server.get("/", function (req, res) {
 server.post("/add", function (req, res) {
 	console.log("Adding file");
 	var url = req.body.url;
-	console.log(url);
+	var id = req.body.id;
 	//relay to picui-core and then send back the body, which will be OK or something
 	request.post({
 		uri: coreServer+"/add",
-		body: JSON.stringify({'url' : url})
+		body: JSON.stringify({'url' : url, 'treeName' : id})
 	}, function (error, response, body) {
 		console.log("sent woo");
-		res.send(body); //maybe should be res.send(response)?
+		res.send(body);
 	});
 });
 
 server.post("/match", function (req, res) {
 	console.log("Matching color(s)");
 	var colors = req.body.colors;
+	var id = req.body.id;
+	var depth = req.body.depth;
 	console.log(colors);
 	//relay this to piqui-core, send back a list of images
 	request.post({
 		uri: coreServer+"/match",
-		body: JSON.stringify({'colors' : colors})
+		body: JSON.stringify({'colors' : colors, 'limit' : depth, 'treeName' : id })
 	}, function (error, response, body) {
 		console.log("sent");
 		res.send(body);
 	});
 });
 
-server.listen(80)
+io.sockets.on('connection', function (socket) {
+	console.log("Someone has joined");
+	clients[socket.id] = socket;
+	socket.emit('id', socket.id);
+
+	socket.on('disconnect', function () {
+		console.log("Someone has left");
+		delete clients[socket.id]
+	});
+});
+
+server.listen(80);
 console.log("Express server started");
