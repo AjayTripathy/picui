9e348402adb32ad152f0dae54c1572c9fc61a82c
AjayTripathy <ubuntu@ip-10-122-215-165.ec2.internal>
2012-05-24 03:22:21 +0000

    unit tested all server components

--- picui-core/server.py
+++ picui-core/server.py
@@ -19,17 +19,16 @@ class add:
     url = str(i.url)
     treeName = str(i.treeName)
     colorListYUV = paletteAnalysis(url)
-    for YUV in colorListYUV:
-      store(YUV, {'url': url}, treeName)
-    return 'ok'
+    for color in colorListYUV:
+      store(color['value'], {'url': url}, treeName)
+    return {'status':'ok'}
   def GET(self):
     i = web.input()
     url = str(i.url)
     treeName = str(i.treeName)
-    #colorListYUV = paletteAnalysis(url)
-    colorListYUV = [ {'value': (1,2)} , {'value': (4,7)}, {'value': (3, 4)} ]
+    colorListYUV = paletteAnalysis(url)
+    #colorListYUV = [ {'value': (1,2)} , {'value': (4,7)}, {'value': (3, 4)} ]
     for color in colorListYUV:
-      print "here's a new color"
       store(color['value'], {'url': url}, treeName)
     callback = str(i.callback)
     return "%s(%s)" % (callback, json.dumps({'status': 'ok'}))
@@ -38,23 +37,27 @@ class match:
   def GET(self):
     i = web.input()
     colors = str(i.colors)
+    colors = json.loads(colors)
     limit = int(i.limit)
     treeName = str(i.treeName)
     for c in colors:
       RGB = imaging.hex_to_rgb(c)
       color = imaging.rgb_to_yuv(RGB)
       response = lookup(color['value'], limit, treeName)
+      response = json.dumps(response)
     if(i.callback != None):
       callback = str(i.callback)
-      response = json.dumps(response)
       return "%s(%s)" % (callback, response)
     else:
       return response
 
 def lookup(point, limit, treeName):
   tree = db[treeName]
-  nearest = tree.query(point, t=4)
-  return nearest.data
+  nearest = tree.query(point, t=limit)
+  retList = []
+  for point in nearest:
+    retList.append(point.data)
+  return retList
   
 
 def store(tup,data,treeName):
@@ -69,12 +72,13 @@ def store(tup,data,treeName):
     db[treeName] = tree
   db.sync() 
 
+
 def paletteAnalysis(url):
-  colorListRGB = fetch_and_extract_colors(url)
+  colorListRGB = imaging.fetch_and_extract_colors(url)
   colorListYUV = []
-  for color in colorList:
+  for color in colorListRGB:
     #construct YUV object
-    YUV = imaging.rgb_to_yuv(color['value'])
+    YUV = imaging.rgb_to_yuv(color)
     colorListYUV.append(YUV)
   return colorListYUV
 
